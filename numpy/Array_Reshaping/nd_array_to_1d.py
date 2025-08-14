aabf75ad9484c4226a97b56c3b496f208d772fe9
emamulazam <emamulazam04@gmail.com>
2025-08-15 02:34:25 +0600

    08 done

---
+++ numpy/Array_Reshaping/nd_array_to_1d.py
@@ -0,0 +1,4 @@
+import numpy as np
+arr = np.array([[1,5,9],[3,5,7]])
+new_arr = arr.reshape(-1)
+print(new_arr)
---
+++ numpy/Array_Shape/shape.py
@@ -0,0 +1,3 @@
+import numpy as np
+arr = np.array([[1,2,3,4],[8,5,2,3]])
+print(arr.shape)
