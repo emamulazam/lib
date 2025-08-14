5cbe5ac681d280575ceaf5340b2e3d373292b3d2
emamulazam <emamulazam04@gmail.com>
2025-08-14 15:21:14 +0600

    fix error

--- numpy/Data_Type/creat_with_data_type01.py
+++ numpy/Data_Type/creat_with_data_type01.py
@@ -1,18 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
-
-#plot 1:
-x = np.array([0, 1, 2, 3])
-y = np.array([3, 8, 1, 10])
-
-plt.subplot(1, 2, 1)
-plt.plot(x,y)
-
-#plot 2:
-x = np.array([0, 1, 2, 3])
-y = np.array([10, 20, 30, 40])
-
-plt.subplot(1, 2, 2)
-plt.plot(x,y)
-
-plt.show()
+arr = np.array([2,5,6,7,8], dtype='i4')
+print(arr)
